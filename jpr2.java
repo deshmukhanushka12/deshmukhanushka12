f8a4675e30a87f83c9f7ba53f9a9a62865896c67
deshmukhanushka12 <88920814+deshmukhanushka12@users.noreply.github.com>
2021-08-14 12:04:47 +0530

    Delete jpr2.java

--- jpr2.java
+++
@@ -1,7 +0,0 @@
-package com.google;
-
-public class jpr2 {
-    public static void main(String[] args) {
-        System.out.println(" \" can we print '\\' with System.out.println() statement? \" ");
-    }
-}
