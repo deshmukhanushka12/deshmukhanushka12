2bac994fab00e40f231a086765c4178711668c11
deshmukhanushka12 <88920814+deshmukhanushka12@users.noreply.github.com>
2021-08-18 10:10:50 +0530

    Create Ass2.java

---
+++ Ass2.java
@@ -0,0 +1,13 @@
+package com.google;
+
+public class Ass2 {
+    public static void main(String[] args) {
+        String x="12-08-2003";
+        // using split method on string object...it return array of splited characters
+        String d=x.split("-")[0];// extract date
+        String m=x.split("-")[1];// extract month
+        String y=x.split("-")[2];// extract year
+
+        System.out.println("Year : "+y);
+    }
+}
